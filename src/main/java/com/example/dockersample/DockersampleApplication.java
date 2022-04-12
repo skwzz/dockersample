9658d18ef29e1808460b078fa7bbea904444b318
KiwoongSon <skw9108@ibizsoftware.net>
2022-04-12 09:49:52 +0900

    Initial commit

---
+++ src/main/java/com/example/dockersample/DockersampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockersampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockersampleApplication.class, args);
+    }
+
+}
